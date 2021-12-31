ba3070fd4b07006d4a39050f36f30f0bafde200f
Melike Aslan <91019037+melikeAslan1@users.noreply.github.com>
2021-12-31 11:18:35 +0300

    C# learning

---
+++ Day_1/ilkProgram1/Program.cs
@@ -0,0 +1,297 @@
+﻿using System;
+
+namespace ilkProgram1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //short bSayiMax = short.MaxValue;
+
+            //short bSayiMin = short.MinValue;
+
+            //Console.WriteLine($"short tipinin alabileceği en büyük değer: { bSayiMax}");
+
+            //Console.WriteLine($"short tipinin alabileceği en küçük değer: { bSayiMin}");
+
+            /*char a = 'a';
+
+            string b = "deneme";
+
+            string c = a + b + "sınıf" + 'm';
+
+            Console.WriteLine(c); */
+
+            /* bool b1 = true;
+
+             bool b2 = false;
+
+             bool b3 = 5 > 4;
+
+             Console.WriteLine(b3); */
+
+            /*  object a = 10;
+
+              object b = 'k';
+
+              object c = "metin";
+
+              object d = 12.9f;  */
+
+            //string a = 5;
+            //string b = 8;
+
+            //Console.WriteLine(a + b);
+
+            /* string a;
+             a = Console.ReadLine();
+
+             Console.WriteLine(a + "metnini yazdiniz");  */
+
+            /*  int a, b;
+
+              a = 20;
+
+              b = a;
+
+              Console.WriteLine(b);  */
+
+            //float a = 12.5f;
+
+            //Console.WriteLine(a);
+
+            /*  const int a = 5;
+
+              const string b = "deneme";  // sabit tanimlama
+
+              const char c = 's';  */
+
+            //string ad = "deneme\"deneme";
+
+            //Console.WriteLine(ad);
+
+            /*  string a = "asda";
+
+              string b = "gaga";
+              string c = "kasjfda";   */
+
+            //  Console.WriteLine("satır\nYeni satır\nYeni satır");
+
+
+            /*  Console.WriteLine(5.GetType());
+              Console.WriteLine(14.4.GetType());
+
+              byte a = 2;
+              Console.WriteLine(a.GetType());  */
+
+            //Console.Write aynı satıra yazar.
+            //Console.WriteLine imleç bir alt satıra geçer.
+
+            /*Console.Write("ad soyad giriniz  ");
+
+            String a = Console.ReadLine();
+
+            Console.WriteLine(a);  */
+
+            /*
+            Console.Write("ad soyad yazınız: ");
+
+            String a = Console.ReadLine();
+
+            Console.Write("e posta yaznız: ");
+
+            String b = Console.ReadLine();
+
+            Console.Write("numara yazınız: ");
+
+            String c = Console.ReadLine();
+
+            //Console.WriteLine("Ad Soyad: " + a +"\nEposta: "+ b + "\nParola: " + c);
+
+            //Console.WriteLine("ad soyad" + a);
+
+            Console.WriteLine($"{a}\n{b}\n{c}");  */
+
+            /* var a = 5;
+             Console.WriteLine(a.GetType());
+
+             var b = "melike";
+             Console.WriteLine(b.GetType());
+
+             var c = 2.4f;
+             Console.WriteLine(c.GetType());  */
+
+            //bool b1 = 1 > 2;
+
+            //bool b2=true;
+
+            //Console.WriteLine(b1);
+
+            //Console.WriteLine(b2);
+
+
+
+            /*  //Char a='a';
+              float a = 5.4f;
+              //int a = 55;
+
+              float b = 4.55f;
+
+              //int b = 4;
+
+              //String b="metin";
+
+              var c = a + b;
+
+              Console.WriteLine(c); */
+
+
+            /*  //string olarak verilen iki değişkeni topla integer a ata.
+
+              String a = "1";
+
+              String b = "2";
+
+              int c= a+b;
+
+              Console.WriteLine(c);   */
+
+
+            /*Console.WriteLine("Deneme \"örnek\" deneme");
+
+            string ad = "deneme\"deneme";
+             Console.WriteLine(ad);   */
+
+            //Console.WriteLine("1.ifadeyi giriniz: ");
+            //var a = Console.ReadLine();
+
+            //Console.WriteLine("2.ifade: ");
+            //var b = Console.ReadLine();
+
+            //Console.WriteLine("3.ifade: ");
+            //var c = Console.ReadLine();
+
+            //Console.WriteLine("1.ifade: " + a + " 2.ifade: " + b + " 3.ifade: " + c);
+
+            /* string yol = @"Windows\Program Files";  //sadece cift tırnakta gecerli değil.
+             Console.WriteLine(yol);  */
+
+          /*  var a = 6;
+            String b = "metin";
+            float c = 23.6f;
+            char d = 'm';
+
+            Console.WriteLine("a nın veri tipi: "+a.GetType()+"\nb nin veri tipi: " + b.GetType()+ "\nc nin veri tipi "+c.GetType()+ "\nd nin veri tipi: " +d.GetType());  */
+            
+
+
+            //2.GÜN
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
