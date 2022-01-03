14f0bc157b9087a52ccc4fc1b68abe4c372fa659
Melike Aslan <91019037+melikeAslan1@users.noreply.github.com>
2022-01-03 11:30:10 +0300

    derss

---
+++ W1_Day1/W1_Day1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace W1_Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+
+
+        }
+    }
+}
