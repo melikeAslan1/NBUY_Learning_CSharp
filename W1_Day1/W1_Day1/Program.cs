ad252f5e1fa58f7056f952cff04037fc6c7b7a2c
Melike Aslan <91019037+melikeAslan1@users.noreply.github.com>
2022-01-03 15:01:38 +0300

    son hali

--- W1_Day1/W1_Day1/Program.cs
+++ W1_Day1/W1_Day1/Program.cs
@@ -6,39 +6,91 @@ namespace W1_Day1
     {
         static void Main(string[] args)
         {
-            sbyte a = -45;
-            a = 75;
-            Console.WriteLine(a);
+            //sbyte a = -45;
+            //a = 75;
+            //Console.WriteLine(a);
 
 
-            // cw + 2 kere tab
+            //// cw + 2 kere tab
 
-            int sayi = int.MinValue;
-            Console.WriteLine($"int min: {sayi}");
+            //int sayi = int.MinValue;
+            //Console.WriteLine($"int min: {sayi}");
 
-            decimal decimalSayiMax = decimal.MaxValue;
-            Console.WriteLine("decimal tipinin alabileceği en büyük değer: "+ decimalSayiMax);
-
-
-            char ab = 'b';
-            Console.WriteLine(ab.GetType());
-
-            var k = 33;
-            Console.WriteLine(k.GetType());
-
-            Int32 i32 = 99;
-            Int16 i16 = 88;
-
-            Console.WriteLine("i32 tipi: "+ i32.GetType());
-            Console.WriteLine("i16 tipi: " + i16.GetType());
-
-            Int32 int32maxValue = Int32.MaxValue;
-            Int16 int16maxValue = Int16.MaxValue;
-
-            Console.WriteLine("int32 tipinin alabileceği en büyük değer: "+int32maxValue);
-            Console.WriteLine("int16 tipinin alabileceği en büyük değer: " + int16maxValue);
+            //decimal decimalSayiMax = decimal.MaxValue;
+            //Console.WriteLine("decimal tipinin alabileceği en büyük değer: "+ decimalSayiMax);
 
 
+            //char ab = 'b';
+            //Console.WriteLine(ab.GetType());
+
+            //var k = 33;
+            //Console.WriteLine(k.GetType());
+
+            //Int32 i32 = 99;
+            //Int16 i16 = 88;
+
+            //Console.WriteLine("i32 tipi: "+ i32.GetType());
+            //Console.WriteLine("i16 tipi: " + i16.GetType());
+
+            //Int32 int32maxValue = Int32.MaxValue;
+            //Int16 int16maxValue = Int16.MaxValue;
+
+            //Console.WriteLine("int32 tipinin alabileceği en büyük değer: "+int32maxValue);
+            //Console.WriteLine("int16 tipinin alabileceği en büyük değer: " + int16maxValue);
+
+            /* bool devamEdilsinMi = false;
+             bool bb = (1 == 2);            */
+
+            //kontol + k + u comment i açar.
+            //kontrol k +c comment yapar.
+
+            //ad soyad yaş email
+
+            /*   Console.WriteLine("ad Soyad giriniz: ");
+               String adSoyad = Console.ReadLine();
+
+               Console.WriteLine("yaşınızı giriniz: ");
+               int yas = Convert.ToInt32(Console.ReadLine());
+
+               Console.WriteLine("e mail adresinizi giriniz: ");
+               string email = Console.ReadLine();
+
+               Console.WriteLine("adınız ve soyadınız: "+adSoyad+"\nyaşınız: "+yas+"\nMail adresiniz: "+email);                     */
+
+
+            //object gelenDegisken = 99;
+            //Console.WriteLine("Genel Değişken: " + gelenDegisken);
+            //gelenDegisken = "Veysel";
+            //Console.WriteLine("Genel Degişken: " + gelenDegisken);
+            //gelenDegisken = 123.45f;
+            //Console.WriteLine("Genel Degişken: " + gelenDegisken);
+
+            /*    Console.WriteLine("sayi giriniz: ");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+
+                if(sayi%2==0)
+                {
+                    Console.WriteLine("sayi 2 ye bölünebilir.");
+                }
+                 if(sayi%3==0){
+                    Console.WriteLine("sayi 3 e bölünebilir.");
+                }
+                 if(sayi%5==0)
+                    {
+                    Console.WriteLine("sayi 5 e tam bölünür.");
+                    }                                                        */
+
+            //if lerde bütün sartlara bakar. else ifte olan sarttan sonraki şartlara bakmaz artık.
+
+            //parasal şeylerde en garanti decimal dir.
+
+            double d = 4.5;
+            double kdv = d * 18 / 118;
+
+            Console.WriteLine("d= " + kdv.ToString());
+
+            int kurus = Convert.ToInt32()
+            Console.WriteLine("kurus= " + kurus);
 
 
 
@@ -47,6 +99,13 @@ namespace W1_Day1
 
 
 
+
+
+
+
+
+
+            Console.ReadLine();
 
         }
     }
