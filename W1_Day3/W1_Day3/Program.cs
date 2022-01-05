84590f1f357d9b01b1b21e8331f3b14c6b1f2b26
Melike Aslan <91019037+melikeAslan1@users.noreply.github.com>
2022-01-05 08:51:20 +0300

    commit 08.51

---
+++ W1_Day3/W1_Day3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace W1_Day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
