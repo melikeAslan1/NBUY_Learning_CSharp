58684bc6c2f5670032421365bba81e6f77d289f7
Melike Aslan <91019037+melikeAslan1@users.noreply.github.com>
2022-01-06 10:37:05 +0300

    commit 10.36

---
+++ W2_Day4/W2_Day4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace W2_Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*  Console.WriteLine("***************************************************");
+                Console.WriteLine("* Network Akademi                                 *");
+
+                int sayac = 1;
+
+                while(sayac<=17)
+                {
+                    Console.WriteLine("*                                                 *");
+                    sayac++;
+                }
+
+                Console.WriteLine("***************************************************");
+                Console.ReadLine();                                                                       */
+
+            // s.Substring(0,10);       substring 0 dan itibaren 10 taneyi getir yapmak.
+
+
+            /*  string s="123456789";
+              Console.WriteLine(s.Substring(0,5));
+              Console.WriteLine();                              */
+
+            // en ve yükseklik klavyeden gir, zemin lacivert yazılar sarı olsun.
+
+          /*  Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+            Console.WriteLine("en: ");
+            int en = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("boy: ");
+            int boy = Convert.ToInt32(Console.ReadLine());                  */
+
+
+
+
+
+
+
+
+        }
+    }
+}
