33d37adf93ebf779f1e20bd5871f02c18c61fc43
Melike Aslan <1melike2@gmail.com>
2022-01-10 21:14:08 +0300

    ilk commit

---
+++ W2_Day1_calisma/W2_Day1_calisma/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace W2_Day1_calisma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
