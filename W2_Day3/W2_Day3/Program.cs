f6ef26ea13a88893ac2bd97a6b0aa8e5722a5e7f
Melike Aslan <91019037+melikeAslan1@users.noreply.github.com>
2022-01-12 09:48:27 +0300

    ilk commit

---
+++ W2_Day3/W2_Day3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace W2_Day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine("girdiğiniz sayi: "+ sayiGir());
+
+            //Console.WriteLine("sayi giriniz: ");
+            //int sayi = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("girdiğiniz sayi: " + sayiGir2(sayi));
+
+            Console.WriteLine(faktöriyel());
+        }
+
+        //sayi girisini otomatikleştiren metodu yazınız.  kullanıcıdan alınan int tipindeki sayıyı döndürecek. 
+
+        static int sayiGir()
+        {
+            Console.WriteLine("sayi giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            return sayi;
+
+        }
+
+        static int sayiGir2(int sayi)
+        {
+            return sayi;
+
+        }
+
+        //parametre olarak aldığı sayının faktöriyelini alıp döndüren metot.
+
+        static int faktöriyel()
+        {
+            Console.WriteLine("sayi giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            int toplam = 1;
+            for (int i = 2; i <= sayi; i++)
+            {
+                toplam *= i;
+            }
+
+            return toplam;
+        }
+    }
+}
