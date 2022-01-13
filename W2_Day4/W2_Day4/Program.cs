60980c6cbd27d5a4e9f7dc8bb2cf365a62a55ae5
Melike Aslan <91019037+melikeAslan1@users.noreply.github.com>
2022-01-13 11:43:14 +0300

    ilk commit

---
+++ W2_Day4/W2_Day4/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace W2_Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*   //sayı tahmin oyunu
+               Random r = new Random();
+               int sayi = r.Next(1, 1001);
+               List<int> tahminler = new List<int>();
+
+
+
+               for (int i = 1; i <= 10; i++)
+               {
+               Console.WriteLine("tahmin yapınız: ");
+               int tahmin = Convert.ToInt32(Console.ReadLine());
+
+
+                   if (tahmin < sayi)
+                   {
+                       Console.WriteLine("tahmininiz küçük");
+                       tahminler.Add(tahmin);
+
+                   }
+                   else if(tahmin>sayi)
+                   {
+                       Console.WriteLine("tahmininiz büyük");
+                       tahminler.Add(tahmin);
+                   }
+                   else 
+                   {
+                       Console.WriteLine("Tebrikler tutturdunuz.");
+                       break;
+                   }
+               }
+               Console.WriteLine("tutturamadınız sayi "+sayi+" idi.");
+               Console.WriteLine(" ");
+               Console.WriteLine("basarısız tahminleriniz: ");
+
+               foreach (var item in tahminler)
+               {
+                   Console.WriteLine(item+" ");
+               }                                                       */
+
+
+           /* // tamamını  yaz.
+
+            List<int> Liste1 = new List<int>();
+            List<int> Liste2 = new List<int>();
+            List<int> Liste3 = new List<int>();
+
+            Random r = new Random();
+           
+            for (int i = 1; i <= 10; i++)
+            {
+                Liste1.Add(r.Next(1, 21));
+                Liste2.Add(r.Next(1, 21));
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+
+                if (Liste1[i] != Liste2[i])
+                {
+
+                    Liste3.Add(Liste1[i]);
+                }
+            }
+
+            Console.Write("Liste1= ");
+            foreach (var item in Liste1)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine("Liste2: ");
+            foreach (var item in Liste2)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine("Liste3: ");
+            foreach (var item in Liste3)
+            {
+                Console.Write(item + " ");
+            }                                     */
+
+
+           
+
+        }
+             //Kendisine gönderilen double dizisini float dizisine ceviren MakeItFloat isimli metodu yazınız.
+
+           static int[] 
+
+
+    }
+}
