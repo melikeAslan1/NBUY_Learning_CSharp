24ab420ecf6e5e298b1879e8d49089f550d00573
Melike Aslan <91019037+melikeAslan1@users.noreply.github.com>
2022-01-14 14:59:01 +0300

    ders sonu

--- W2_Day5/W2_Day5/Program.cs
+++ W2_Day5/W2_Day5/Program.cs
@@ -8,158 +8,162 @@ namespace W2_Day5
         static void Main(string[] args)
         {
 
-            //Console.WriteLine(Topla("1","2","3","4"));
-            //Console.WriteLine(Topla("Korkma","sönmez","bu"));
-            //Console.ReadLine();
-
-            ////referans tipi parametreler
-            //int s1;
-            //int s2;
-            //DegerDegistir(ref s1, ref s2);
-
-            //Console.WriteLine("s1= "+ s1);
-            //Console.WriteLine("s2=" + s2);
-
-            //Console.ReadLine();
 
 
-            /*   int mutlak7 = Math.Abs(-7);
-               Console.WriteLine(mutlak7);
+        //    -ref ve out olunca metot içindeki değişkenin değeri dışarı çıkar.diğer türlü çıkamaz.
 
-               double cos45 = Math.Cos(45);
+          //Console.WriteLine(Topla("1","2","3","4"));
+          //Console.WriteLine(Topla("Korkma","sönmez","bu"));
+          //Console.ReadLine();
 
-               Console.WriteLine(cos45);
+          ////referans tipi parametreler
+          //int s1;
+          //int s2;
+          //DegerDegistir(ref s1, ref s2);
 
-               Console.WriteLine(Math.Sin(45));
+          //Console.WriteLine("s1= "+ s1);
+          //Console.WriteLine("s2=" + s2);
 
-               Console.WriteLine(Math.Tan(45));
-
-               Console.WriteLine(Math.Max(4,5));
-
-               Console.WriteLine(Math.Min(4,5));
-
-               Console.WriteLine(Math.Pow(2,8));
-
-               Console.WriteLine(Math.Sqrt(36));
-
-               Console.WriteLine(Math.Log(100));
-
-               Console.WriteLine(Math.Exp(4));
-
-               Console.WriteLine(Math.Log10(5));
-
-               Console.WriteLine(Math.Log(16,2));
-
-               Console.ReadLine();                           */
+          //Console.ReadLine();
 
 
-            //string birlesim = String.Concat("Network", "Akademi", "303Sabah", 2022);
+          /*   int mutlak7 = Math.Abs(-7);
+             Console.WriteLine(mutlak7);
 
-            //Console.WriteLine(birlesim);
+             double cos45 = Math.Cos(45);
 
-            //Console.ReadLine();
+             Console.WriteLine(cos45);
 
+             Console.WriteLine(Math.Sin(45));
 
-            //string[] dizi = { "Korkma", "sönmez", "bu", "Safaklarda" };
+             Console.WriteLine(Math.Tan(45));
 
-            //birlesim = string.Concat(dizi);
+             Console.WriteLine(Math.Max(4,5));
 
-            //Console.WriteLine(birlesim);
+             Console.WriteLine(Math.Min(4,5));
 
+             Console.WriteLine(Math.Pow(2,8));
 
-            //int[] intDizi = { 1, 2, 3, 4, 5 };
+             Console.WriteLine(Math.Sqrt(36));
 
-            //birlesim = String.Concat(intDizi);
+             Console.WriteLine(Math.Log(100));
 
-            //Console.WriteLine(birlesim);
+             Console.WriteLine(Math.Exp(4));
 
-            //Console.ReadLine();
+             Console.WriteLine(Math.Log10(5));
 
+             Console.WriteLine(Math.Log(16,2));
 
-            //string s1 = "Adana";
-            //string s2 = "Adana";
-
-            //int comp = String.Compare(s1, s2);
-
-            //Console.WriteLine("comp= " + comp);
-
-            //if (comp == 1)
-            //    Console.WriteLine(s1);
-
-            //else
-            //    Console.WriteLine(s2);
-            //Console.ReadLine();
-
-            //for (int i = 0; i < 10; i++)
-            //{
-
-            //Console.Write("String 1: ");
-            //string st1 = Console.ReadLine();
-
-            //Console.Write("String 2: ");
-            //string st2 = Console.ReadLine();
+             Console.ReadLine();                           */
 
 
-            //int a = string.Compare(st1, 0,st2,0,100,true);
+          //string birlesim = String.Concat("Network", "Akademi", "303Sabah", 2022);
 
-            //if (a == -1)
-            //    Console.WriteLine("önce gelen" + st1);
+          //Console.WriteLine(birlesim);
 
-            //else if (comp == 0)
-            //    Console.WriteLine("Aynı sırada: " + st2.ToUpper());
-            //else
-            //    Console.WriteLine("önce gelen" + st2);
-            //Console.ReadLine();
-            //}
+          //Console.ReadLine();
 
 
-            //string metin = "BEŞİKTAŞ";
-            //Console.WriteLine(metin.IndexOf("Ş"));
-            //Console.WriteLine(metin.LastIndexOf("Ş"));
-            //Console.WriteLine(metin.StartsWith("BEŞ"));
-            //Console.WriteLine(metin.StartsWith("Beş"));
-            //Console.WriteLine(metin.EndsWith("TAŞ"));
+          //string[] dizi = { "Korkma", "sönmez", "bu", "Safaklarda" };
 
-            //metin = "       ____FENERBAHÇE____   ";
-            //Console.WriteLine(metin.Trim('_', ' ', 'E');
+          //birlesim = string.Concat(dizi);
 
-            //metin = "Merhaba Dünya";
-            //Console.WriteLine(metin.PadLeft(20,'.'));
-            //Console.WriteLine(metin.PadRight(20,'.'));
-
-            /*   string youtubeStr = "https://www.youtube.com/watch?v=4WYBWi0-D3o";
-               string[] adres = youtubeStr.Split('/');
-
-               Console.WriteLine("adres[0]= "+ adres[0]);
-               Console.WriteLine("adres[1]= " + adres[1]);
-               Console.WriteLine("adres[2]= " + adres[2]);
-
-               string cumle = "Merhaba arkadaşlar bugün hava çok güzel";
-
-               string[] kelimeler = cumle.Split(' ');
-
-               foreach (var kelime in kelimeler)
-               {
-                   Console.WriteLine(kelime);
-               }
-
-               string yeniCumle = string.Join("/", kelimeler);
-
-               yeniCumle = yeniCumle.ToUpper();
-               Console.WriteLine(yeniCumle);
-
-               yeniCumle = yeniCumle.ToLower();
-               Console.WriteLine(yeniCumle);           
-
-               yeniCumle = yeniCumle.Remove(3, 1);
-               Console.WriteLine(yeniCumle);
+          //Console.WriteLine(birlesim);
 
 
-               Console.WriteLine(yeniCumle.Insert(6,"123"));
-               Console.WriteLine(yeniCumle.Replace("arkadaşlar","gençler"));                   */
+          //int[] intDizi = { 1, 2, 3, 4, 5 };
+
+          //birlesim = String.Concat(intDizi);
+
+          //Console.WriteLine(birlesim);
+
+          //Console.ReadLine();
 
 
-            reverse("tugba");
+          //string s1 = "Adana";
+          //string s2 = "Adana";
+
+          //int comp = String.Compare(s1, s2);
+
+          //Console.WriteLine("comp= " + comp);
+
+          //if (comp == 1)
+          //    Console.WriteLine(s1);
+
+          //else
+          //    Console.WriteLine(s2);
+          //Console.ReadLine();
+
+          //for (int i = 0; i < 10; i++)
+          //{
+
+          //Console.Write("String 1: ");
+          //string st1 = Console.ReadLine();
+
+          //Console.Write("String 2: ");
+          //string st2 = Console.ReadLine();
+
+
+          //int a = string.Compare(st1, 0,st2,0,100,true);
+
+          //if (a == -1)
+          //    Console.WriteLine("önce gelen" + st1);
+
+          //else if (comp == 0)
+          //    Console.WriteLine("Aynı sırada: " + st2.ToUpper());
+          //else
+          //    Console.WriteLine("önce gelen" + st2);
+          //Console.ReadLine();
+          //}
+
+
+          //string metin = "BEŞİKTAŞ";
+          //Console.WriteLine(metin.IndexOf("Ş"));
+          //Console.WriteLine(metin.LastIndexOf("Ş"));
+          //Console.WriteLine(metin.StartsWith("BEŞ"));
+          //Console.WriteLine(metin.StartsWith("Beş"));
+          //Console.WriteLine(metin.EndsWith("TAŞ"));
+
+          //metin = "       ____FENERBAHÇE____   ";
+          //Console.WriteLine(metin.Trim('_', ' ', 'E');
+
+          //metin = "Merhaba Dünya";
+          //Console.WriteLine(metin.PadLeft(20,'.'));
+          //Console.WriteLine(metin.PadRight(20,'.'));
+
+          /*   string youtubeStr = "https://www.youtube.com/watch?v=4WYBWi0-D3o";
+             string[] adres = youtubeStr.Split('/');
+
+             Console.WriteLine("adres[0]= "+ adres[0]);
+             Console.WriteLine("adres[1]= " + adres[1]);
+             Console.WriteLine("adres[2]= " + adres[2]);
+
+             string cumle = "Merhaba arkadaşlar bugün hava çok güzel";
+
+             string[] kelimeler = cumle.Split(' ');
+
+             foreach (var kelime in kelimeler)
+             {
+                 Console.WriteLine(kelime);
+             }
+
+             string yeniCumle = string.Join("/", kelimeler);
+
+             yeniCumle = yeniCumle.ToUpper();
+             Console.WriteLine(yeniCumle);
+
+             yeniCumle = yeniCumle.ToLower();
+             Console.WriteLine(yeniCumle);           
+
+             yeniCumle = yeniCumle.Remove(3, 1);
+             Console.WriteLine(yeniCumle);
+
+
+             Console.WriteLine(yeniCumle.Insert(6,"123"));
+             Console.WriteLine(yeniCumle.Replace("arkadaşlar","gençler"));                   */
+
+
+          reverse("tugba");
 
 
 
