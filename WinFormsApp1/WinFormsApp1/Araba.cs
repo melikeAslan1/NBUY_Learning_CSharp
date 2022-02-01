eeda158a2642186dba9df6263153f93a6da66a71
Melike Aslan <91019037+melikeAslan1@users.noreply.github.com>
2022-02-01 14:59:15 +0300

    31 ocak pazartesi

---
+++ WinFormsApp1/WinFormsApp1/Araba.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    class Araba
+    {
+
+        public int ID;
+        public string Marka;
+        public string Model;
+
+        public Araba()
+        {
+            MessageBox.Show("Yapıcı metot çalıştı");
+
+            /* YAPICI METOTLAR: CONSTRUCTOR
+             * Metot adı sınıf adı ile aynı olmalıdır. Public olmalıdır.
+             * Tanımlanmasa da boş hali arka planda çalışır.
+             */
+
+        }
+
+        public Araba(int ID, string marka, string model)
+        {
+            MessageBox.Show("3 parametreli yapıcı metot çalıştı");
+            this.ID = ID;
+            Marka = marka;
+            Model = model;
+        }
+
+
+        public void BilgiYaz()
+        {
+            MessageBox.Show("Marka: " + this.Marka);
+            MessageBox.Show("Model: " + this.Model);
+
+            //this kullanılıyorsa property belirtilmiştir.
+        }
+
+    }
+}
---
+++ WinFormsApp1/WinFormsApp1/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+
+        
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Araba arabaNesnesi = new Araba();
+            //arabaNesnesi.ID = 1;
+            //arabaNesnesi.Marka = "Mazda";
+            //arabaNesnesi.Model = "6.0";
+            //arabaNesnesi.BilgiYaz();
+
+            //Araba araba2 = new Araba(2, "Mercedes", "S500");
+
+            Ogrenci o = new Ogrenci();
+            //o.TcKimlikNo = 3456;
+            //o.Ad = "Melike";
+            //o.Soyad = "Aslan";
+            //o.SinifOgretmeni = "Veysel";
+            //o.OkulNo = "2312";
+
+            //o.BilgiYaz();
+
+            Ogretmen ogretmen = new Ogretmen();
+
+            ogretmen.OgrenciListele();
+
+
+        }
+
+        
+    }
+}
